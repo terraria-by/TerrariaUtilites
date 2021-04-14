4af1d82f6699df971f904aee7b09913103b2f009
terraria-by <User@comp85 MINGW64 /$ git config --global user.name terraria-byUser@comp85 MINGW64 /$ git config --global user.email>
2021-04-14 11:14:04 +0300

    upload

---
+++ TerrariaUtilites/GGPO.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using TShockAPI;
+using TShockAPI.DB;
+using Terraria;
+using TerrariaApi.Server;
+
+namespace TerrariaUtilites {
+    [ApiVersion(2, 1)]
+    public class GGPO : TerrariaPlugin {
+        public override string Name => "GGPO";
+        public override string Description => "Displays the last run of players of a certain group ";
+
+        public GGPO(Main main) : base(main) { }
+
+        public override void Initialize() {
+            Commands.ChatCommands.Add(new Command("kd.gonline", Com, "gonline"));
+        }
+
+        public static void Com(CommandArgs args) {
+            if (args.Parameters.Count < 1) {
+                args.Player.SendErrorMessage("You must enter the name of a particular group. (Not default) ");
+                return;
+            }
+
+            Group group = TShock.Groups.GetGroupByName(args.Parameters[0]);
+
+            if (group.Name.ToLower() == TShock.Config.DefaultRegistrationGroupName.ToLower()) {
+                args.Player.SendErrorMessage("You must enter the name of a particular group. [c/ff0000:(Not default)]");
+                return;
+            }
+
+            string tt = TimeZone.CurrentTimeZone.GetUtcOffset(DateTime.Now).Hours.ToString("+#;-#");
+
+            using (var query = TShock.DB.QueryReader("SELECT * FROM Users WHERE Usergroup=@0", group.Name)) {
+                while (query.Read()) {
+                    try {
+                        UserAccount account = TShock.UserAccounts.GetUserAccountByName(query.Get<string>("Username"));
+
+                        //DateTime time = DateTime.Parse(account.LastAccessed).ToLocalTime();
+                        DateTime time;
+                        if (DateTime.TryParse(account.LastAccessed, out time)) {
+                            time = DateTime.Parse(account.LastAccessed).ToLocalTime();
+                            args.Player.SendInfoMessage("[c/082567:{0}]'s last login occured {1} {2} UTC{3}.", account.Name, time.ToShortDateString(), time.ToShortTimeString(), tt);
+                        }
+                    }
+                    catch (Exception ex) {
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ TerrariaUtilites/IgnoreTheMismatchedTiles.cs
@@ -0,0 +1,73 @@
+﻿using TShockAPI;
+using Terraria;
+using TerrariaApi.Server;
+using System.IO;
+
+namespace TerrariaUtilites {
+    [ApiVersion(2, 1)]
+    public class IgnoreTheMismatchedTiles : TerrariaPlugin {
+        public override string Author {
+            get {
+                return "Zoom L1 | Colag";
+            }
+        }
+        public override string Name {
+            get {
+                return "Ignore the mismatched tiles";
+            }
+        }
+
+        public IgnoreTheMismatchedTiles(Main game) : base(game) { }
+
+        public override void Initialize() {
+            GetDataHandlers.TileEdit += OnTileEdit;
+            GetDataHandlers.PlaceObject += OnPlaceObject;
+            ServerApi.Hooks.NetGetData.Register(this, OnGetData);
+        }
+
+        public static void OnTileEdit(object sender, GetDataHandlers.TileEditEventArgs args) {
+            if (args.Action == GetDataHandlers.EditAction.PlaceTile) {
+                if (Main.tile[args.X, args.Y] != null && Main.tile[args.X, args.Y].type == 127) {
+                    args.Handled = true;
+                    return;
+                }
+                if (args.Player.SelectedItem.placeStyle != args.Style) {
+                    args.Handled = true;
+                    return;
+                }
+            }
+        }
+
+        public static void OnPlaceObject(object sender, GetDataHandlers.PlaceObjectEventArgs args) {
+            if (Main.tile[args.X, args.Y] != null && Main.tile[args.X, args.Y].type == 127) {
+                args.Handled = true;
+                return;
+            }
+            if (args.Player.SelectedItem.placeStyle != args.Style) {
+                args.Handled = true;
+                return;
+            }
+        }
+
+        public static void OnGetData(GetDataEventArgs e) {
+            if (e.MsgID == (PacketTypes)34) {
+                TSPlayer player = TShock.Players[e.Msg.whoAmI];
+                using (var data = new BinaryReader(new MemoryStream(e.Msg.readBuffer, e.Index, e.Length))) {
+                    data.ReadByte();
+                    int x = (int)data.ReadInt16();
+                    int y = (int)data.ReadInt16();
+                    short style = data.ReadInt16();
+                    if (Main.tile[x, y] != null && Main.tile[x, y].type == 127) {
+                        e.Handled = true;
+                        return;
+                    }
+                    if (player.SelectedItem.placeStyle != style) {
+                        e.Handled = true;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+}
